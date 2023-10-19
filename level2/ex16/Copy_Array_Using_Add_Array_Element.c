afec962028cb66b5c399422270c7f3c35a772b0a
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2023-10-19 17:48:33 +0000

    Solving Problem 16: Program to Read lenght array and fill Array With Random Number from 1 to 100, Copy it to another array using function add_array_Element And Print it

---
+++ level2/ex16/Copy_Array_Using_Add_Array_Element.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int Read_Number()
+{
+	int Number = 0;
+
+	do
+	{
+		printf("Pleas enter the Number ?\n");
+		scanf("%d", &Number);
+
+	}while(Number <= 0);
+	return (Number);
+}
+int Random_Number(int From, int To)
+{
+	int RandNum = rand() % (To - From + 1) + From;
+	return (RandNum);
+}
+int *Fill_Array_With_Random_Number(int lenght)
+{
+	int *Arr = (int *)malloc(sizeof(int) * lenght + 1);
+	
+	if(Arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for(int i = 0; i < lenght; i++)
+	{
+		Arr[i] = Random_Number(1, 100);
+	}
+	return (Arr);
+}
+void Add_Array_Element(int Number,int *Arr2, int Count)
+{
+	Count++;
+	Arr2[Count - 1] = Number;
+}
+int *Copy_Array_To_Another_Array(int *Arr, int *Arr2, int lenght)
+{
+	for(int i = 0; i < lenght; i++)
+	{
+		Add_Array_Element(Arr[i], Arr2, i);	
+	}
+	return (Arr2);
+}
+void Print_Array(int *Arr, int lenght)
+{
+	for(int i = 0; i < lenght; i++)
+	{
+		printf("%d ", Arr[i]);
+	}
+	putchar('\n');
+}
+int main()
+{
+	srand((unsigned) time(NULL));
+
+	int lenght = Read_Number();
+	int *Arr = Fill_Array_With_Random_Number(lenght);
+	int *Arr2 = (int *)malloc(sizeof(int) * (lenght + 1));
+
+	Copy_Array_To_Another_Array(Arr, Arr2, lenght);
+
+	printf("Array Befor Copy Element : ");
+	Print_Array(Arr, lenght);
+
+	printf("Array After Copy elemnt : ");
+	Print_Array(Arr2, lenght);
+
+	free(Arr);
+	free(Arr2);
+	return (0);
+}
