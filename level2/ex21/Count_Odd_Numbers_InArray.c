775bac70dff58a66fae726746fbd64101a07d19d
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2023-10-21 17:45:08 +0000

    Solving Problem 21: Fill Array With Random Numbers And Print the Count Of Odd Numbers

---
+++ level2/ex21/Count_Odd_Numbers_InArray.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int Read_Number()
+{
+	int Number = 0;
+	do
+	{
+		printf("Pleas enter the Number Positive ?\n");
+		scanf("%d", &Number);
+	
+	}while(Number <= 0);
+	
+	return (Number);
+}
+int Random_Number(int From, int To)
+{
+	int RandNum = rand() % (To - From + 1) + From;
+	return (RandNum);
+}
+int *Fill_Array_With_Random_Number(int lenght)
+{
+	int *Arr = (int *)malloc(sizeof(int) * lenght + 1);
+	
+	if(Arr == NULL)
+	{
+		return (NULL);
+	}
+	
+	for(int i = 0; i < lenght; i++)
+	{
+		Arr[i] = Random_Number(1, 100);
+	}
+
+	return (Arr);
+}
+int Count_Odd_Number_In_Array(int *Arr, int lenght)
+{
+	int Count = 0;
+	for(int i = 0; i < lenght; i++)
+	{
+		if(Arr[i] % 2 != 0)
+		{
+			Count++;
+		}
+	}
+	return(Count);
+}
+void Print_Array(int *Arr, int lenght)
+{
+	printf("Array Elements: ");
+	for(int i = 0; i < lenght; i++)
+	{
+		printf("%d ", Arr[i]);
+	}
+	putchar('\n');
+}
+int main()
+{
+	int lenght = Read_Number();
+	int *Arr = Fill_Array_With_Random_Number(lenght);
+	Print_Array(Arr, lenght);
+
+	int Count = Count_Odd_Number_In_Array(Arr, lenght);
+	printf("Odd Numbers Counts Is : %d\n", Count);
+
+	free(Arr);
+	return (0);
+}
