da14157128f1e616d969211517948a119e6788a7
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2023-10-21 19:15:43 +0000

    Solving Problem 23: Fill Array With Random Numbers And Print the Count Of Positive Numbers

---
+++ level2/ex23/Count_Positive_Number_In_Array.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int Read_Number()
+{
+	int Number = 0;
+	do
+	{
+		printf("Pleas enter the Number Positive ?\n");
+		scanf("%d", &Number);
+		
+	}while(Number <= 0);
+	return (Number);
+}
+int Random_Number(int From, int To)
+{
+	int RandNum = rand() % (To - From + 1) + From;
+	return (RandNum);
+}	
+int *Fill_Array_With_Random_Number(int lenght)
+{
+	int *Arr = malloc(sizeof(int) * lenght + 1);
+	if(Arr == NULL)
+	{
+		return (NULL);
+	}
+	for(int i = 0; i < lenght; i++)
+	{
+		Arr[i] = Random_Number(-100, 100);
+	}
+	return (Arr);
+}
+int Count_Positive_Number_InArray(int *Arr, int lenght)
+{
+	int Count = 0;
+	for(int i = 0; i < lenght; i++)
+	{
+		if(Arr[i] >= 0)
+		{
+			Count++;
+		}
+	}
+	return (Count);
+}
+void Print_Array(int *Arr, int lenght)
+{
+	printf("Array Elemnts : ");
+	for(int i = 0; i < lenght; i++)
+	{
+		printf("%d ", Arr[i]);
+	}
+	putchar('\n');
+}
+int main()
+{
+	int lenght = Read_Number();
+	int *Arr = Fill_Array_With_Random_Number(lenght);
+	Print_Array(Arr, lenght);
+
+	int Count = Count_Positive_Number_InArray(Arr, lenght);
+	printf("Positive Numbers Count Is %d\n", Count);
+
+	free(Arr);
+	return (0);
+}
